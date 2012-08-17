93747f49d2d762b8e8c0056d4efc9f0e998a3436
xapfeltortexp <jan.hoeck@gmx.de>
2012-08-17 21:33:01 +0200

    kick Command added

--- LevelUpGames/src/us/manniL/xapfeltortexp/LevelUpGames/cmd/lkick.java
+++ LevelUpGames/src/us/manniL/xapfeltortexp/LevelUpGames/cmd/lkick.java
@@ -17,41 +17,37 @@ public class lkick extends SubBase {
 	public void run(CommandSender sender, String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("[LevelUpGames] You are not a Player. Join the Server to perform some Commands.");
-			return ;
+			return;
 		}
 		Player player = (Player) sender;
-			if ((args.length > 2) || (args.length == 1)) {
-				plugin.toMuchArgs(player);
-				return;
-			}
-			if (!player.hasPermission("LevelUpGames.kick")) {
-				plugin.noPerms(player);
-				return;
-			}
-			if (args[1].equalsIgnoreCase(player.getName())) {
-				if (this.plugin.players.contains(player.getName())) {
-					player.sendMessage(plugin.prefix + plugin.gray + "You cant kick yourself. Please try /lug leave.");
-					return;
-				}
-				player.sendMessage(plugin.prefix + plugin.gray + "You are not in the LevelUpGames and you cant kick yourself.");
-				return;
-			}
+		if (args.length > 1) {
+			plugin.toMuchArgs(player);
+			return;
+		}
+		if (!player.hasPermission("LevelUpGames.kick")) {
+			plugin.noPerms(player);
+			return;
+		}
+		if (args[0].equalsIgnoreCase(player.getName())) {
+			player.sendMessage(plugin.prefix + plugin.gray + "You cant kick yourself. Please try /lug leave.");
+			return;
+		}
 
-			Player target = Bukkit.getServer().getPlayer(args[1]);
-			if (target == null) {
-				player.sendMessage(plugin.prefix + plugin.blue + args[1] + plugin.gray + " not found.");
-				return;
-			}
+		Player target = Bukkit.getServer().getPlayer(args[0]);
+		if (target == null) {
+			player.sendMessage(plugin.prefix + plugin.blue + target.getName() + plugin.gray + " not found.");
+			return;
+		} else {
 			if (!this.plugin.players.contains(target.getName())) {
 				player.sendMessage(plugin.prefix + plugin.gray + "This Player isnt in the LevelUpGames.");
 				return;
 			}
-			target.teleport(target.getWorld().getSpawnLocation());
-			this.plugin.players.remove(target.getName());
-			this.plugin.limit -= 1;
-			player.sendMessage(plugin.prefix + plugin.gray + "You kicked " + plugin.blue + args[1] + plugin.gray + " from the LevelUpGames.");
-			return;
 		}
-
+		target.teleport(target.getWorld().getSpawnLocation());
+		this.plugin.players.remove(target.getName());
+		this.plugin.limit -= 1;
+		player.sendMessage(plugin.prefix + plugin.gray + "You kicked " + plugin.blue + target.getName() + plugin.gray + " from the LevelUpGames.");
+		return;
 	}
 
+}
