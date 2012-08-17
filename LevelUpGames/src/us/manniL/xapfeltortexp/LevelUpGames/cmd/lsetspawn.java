c5f4ff2c43772dde42d5d567624b1d7acede474d
manniL <manniL@gmx.net>
2012-08-17 21:42:42 +0200

    Listeners + /lug setspawn [radius] added!

--- LevelUpGames/src/us/manniL/xapfeltortexp/LevelUpGames/cmd/lsetspawn.java
+++ LevelUpGames/src/us/manniL/xapfeltortexp/LevelUpGames/cmd/lsetspawn.java
@@ -1,5 +1,8 @@
 package us.manniL.xapfeltortexp.LevelUpGames.cmd;
 
+import java.util.Random;
+
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -18,6 +21,30 @@ public class lsetspawn extends SubBase {
 			sender.sendMessage("[LevelUpGames] You are not a Player. Join the Server to perform some Commands.");
 			return;
 		}
+		Player p = (Player) sender;
+		if(args.length >= 1){
+		 plugin.toMuchArgs(p);
+		}
+		Location l = p.getLocation();
+		int r = Integer.parseInt(args[0])+1;
+		Random r1 = new Random();
+		Random r2 = new Random();
+		int z = r1.nextInt(r);
+		int x = r2.nextInt(r);
+		int sign1 = r1.nextInt(2);
+		int sign2 = r2.nextInt(2);
+		if(sign1 == 1){
+			z = -(z);
+		}
+		if(sign2 == 1){
+			x = -(x);
+		}
+		plugin.getConfig().set("RandomSpawnRadius", r-1);
+		plugin.getConfig().set("RandomSpawn.x",l.getX());
+		plugin.getConfig().set("RandomSpawn.y",l.getY());
+		plugin.getConfig().set("RandomSpawn.z",l.getZ());
+		plugin.saveConfig();
+		sender.sendMessage("");
 	}
 
 }
--- LevelUpGames/src/us/manniL/xapfeltortexp/LevelUpGames/listeners/LevelUpGamesListener.java
+++ LevelUpGames/src/us/manniL/xapfeltortexp/LevelUpGames/listeners/LevelUpGamesListener.java
@@ -1,13 +1,27 @@
 package us.manniL.xapfeltortexp.LevelUpGames.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import us.manniL.xapfeltortexp.LevelUpGames.LevelUpGamesMain;
 
 public class LevelUpGamesListener implements Listener {
-	public LevelUpGamesMain plugin;
+	private LevelUpGamesMain plugin;
 
 	public LevelUpGamesListener(LevelUpGamesMain plugin) {
-		this.plugin = plugin;
+		this.plugin = LevelUpGamesMain.Instance;
 	}
+	@EventHandler
+	public void onDeath(EntityDeathEvent e){
+		
+	}
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e){
+		if(plugin.players.contains(e.getPlayer().getName())){
+			e.getPlayer().teleport(e.getPlayer().getWorld().getSpawnLocation());
+		}
+	}
+	
 }
