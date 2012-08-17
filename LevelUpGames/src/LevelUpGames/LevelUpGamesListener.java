26e22acbbd661ea56d97fac9a0b19c5622f7e246
xapfeltortexp <jan.hoeck@gmx.de>
2012-08-17 14:56:21 +0200

    Release

---
+++ LevelUpGames/src/LevelUpGames/LevelUpGamesListener.java
@@ -0,0 +1,11 @@
+package LevelUpGames;
+
+import org.bukkit.event.Listener;
+
+public class LevelUpGamesListener implements Listener {
+	public LevelUpGamesMain plugin;
+
+	public LevelUpGamesListener(LevelUpGamesMain plugin) {
+		this.plugin = plugin;
+	}
+}
